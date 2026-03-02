0ac84b74f5bff22518a380cee0288004e28d1dfe
Bridionizzz <brunodeby@gmail.com>
2026-03-02 14:28:33 -0300

    Update _app.js

--- projeto-web/pages/_app.js
+++ projeto-web/pages/_app.js
@@ -1,23 +1,15 @@
-// pages/_app.js
 import '../styles/globals.css';
 import { useEffect } from 'react';
-import { Toaster } from 'react-hot-toast';
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    // Aplicar tema salvo
     const isDark = localStorage.getItem('tema-escuro') === 'true';
     if (isDark) {
       document.body.classList.add('dark');
     }
   }, []);
 
-  return (
-    <>
-      <Component {...pageProps} />
-      <Toaster position="top-right" />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
