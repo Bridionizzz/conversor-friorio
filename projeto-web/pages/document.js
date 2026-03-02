3bca6238cc0032a1c4e638a76b8645139cc66993
Bridionizzz <brunodeby@gmail.com>
2026-03-02 14:09:32 -0300

    Update document.js

---
+++ projeto-web/pages/document.js
@@ -0,0 +1,19 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="pt-BR">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="theme-color" content="#2d89ef" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
