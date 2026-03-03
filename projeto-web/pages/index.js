4d0c6d7f56a8bb9324609cbcbe4c50f710c36b6f
Bridionizzz <brunodeby@gmail.com>
2026-03-03 17:06:30 -0300

    Delete projeto-web directory

--- projeto-web/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '40px', textAlign: 'center', fontFamily: 'Arial' }}>
-      <h1>Conversor FRIORIO</h1>
-      <p>🚀 Está funcionando!</p>
-    </div>
-  );
-}
